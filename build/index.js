17584ac04a85ba62c69bb5ebe0398f058b01eda6
Alex <alexshcherb84@gmail.com>
2018-07-24 13:24:07 +0300

    Gradle updates (#29)
    * removed duplicated dependencies
    * moved dependencies from jars to gradle
    * added google play services
    * migrated to oficcial updated OneSignal installation
    * uncomment manifest metadata for plugin js file using 2
    * fix for gradle injections
    * fix dependency
    * added tools:replace=android:theme
    * fix git cache

--- build/index.js
+++ build/index.js
@@ -6,7 +6,11 @@ var path = require("path"),
   copyNotifyIcon = function (app, outputPath, tag, name, large) {
     'use strict';
 
-    var dest_path = path.join(outputPath, "res/drawable-" + tag +
+    var dest_path = path.join(outputPath,
+        "../..",
+        app.manifest.shortName,
+        "tealeaf/src/main",
+        "res/drawable-" + tag +
         "dpi/"),
       android = app.manifest.android,
       icons = android && android.icons,
